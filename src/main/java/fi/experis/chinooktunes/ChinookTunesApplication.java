f65dab755d25de9019c98e4dd1003717a88b2bd2
maija.haka <maija.haka@5CG05206QX.nordic.local>
2021-02-04 14:56:10 +0200

    initial commit

---
+++ src/main/java/fi/experis/chinooktunes/ChinookTunesApplication.java
@@ -0,0 +1,13 @@
+package fi.experis.chinooktunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookTunesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinookTunesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/experis/chinooktunes/ChinookTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.experis.chinooktunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookTunesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
